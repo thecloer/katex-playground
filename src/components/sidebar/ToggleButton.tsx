8f1d9014d6959fc605da81e3621a04a60620adae
cloer <chltjrrb97@gmail.com>
2023-09-18 07:33:35 +0900

    FIX: set tooglebutton's width and heigth

--- src/components/sidebar/ToggleButton.tsx
+++ src/components/sidebar/ToggleButton.tsx
@@ -9,7 +9,7 @@ export default function ToggleButton({ isOpen, toggle }: ToggleButtonProps) {
       className='absolute top-2 right-2 w-8 h-8 p-1 flex items-center justify-center rounded-md border-2 border-teal-500 bg-white hover:bg-teal-400'
       onClick={toggle}
     >
-      <svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' className='h-full w-full'>
         {isOpen ? (
           <path d='M16 8L8 16M8 8L16 16' stroke='currentColor' strokeWidth='2' strokeLinecap='round' />
         ) : (
