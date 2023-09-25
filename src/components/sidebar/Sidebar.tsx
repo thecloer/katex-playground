d2cf457d37d9611a298bb45e8493dcb42faccd07
cloer <chltjrrb97@gmail.com>
2023-09-25 15:20:45 +0900

    ADD: drag resize sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
 import ToggleButton from './ToggleButton';
 import SaveButton from './SaveButton';
 import ItemList from './ItemList';
 import { StorageProvider } from '../../contexts/storageContext';
 import ShareButton from './ShareButton';
 
+const MIN_WIDTH = 288;
+
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen((v) => !v);
+
+  const dragHandler: MouseEventHandler<HTMLDivElement> = (e) => {
+    const target = e.currentTarget.parentElement;
+    if (!target) return;
+
+    const resize: EventListener = (e) => {
+      const right = target.getBoundingClientRect().left + target.getBoundingClientRect().width;
+      const left = (e as MouseEvent).clientX;
+      const width = Math.max(MIN_WIDTH, right - left);
+      target.style.width = `${width}px`;
+    };
+
+    document.addEventListener('mousemove', resize);
+    document.addEventListener('mouseup', () => document.removeEventListener('mousemove', resize));
+  };
 
   return (
     <>
@@ -16,6 +33,7 @@ export default function Sidebar() {
         p-2 w-72 border-l-2 bg-gray-100 
         transition ease-in-out ${isOpen ? '' : 'translate-x-full'}`}
       >
+        <div className='absolute top-0 bottom-0 w-2 cursor-col-resize left-0' onMouseDown={dragHandler} />
         <StorageProvider>
           <div className='flex gap-2 mr-10'>
             <SaveButton />
