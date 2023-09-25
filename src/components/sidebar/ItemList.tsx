313038d541e05a0cf7de7dfbb56f0af10657a832
cloer <chltjrrb97@gmail.com>
2023-09-25 13:58:40 +0900

    UPDATE: hide sidebar scrollbar

--- src/components/sidebar/ItemList.tsx
+++ src/components/sidebar/ItemList.tsx
@@ -4,7 +4,7 @@ import Item from './Item';
 export default function ItemList() {
   const storage = useStorage();
   return (
-    <div className='mt-2 grow overflow-scroll flex flex-col gap-4'>
+    <div className='mt-2 grow flex flex-col gap-4 overflow-y-scroll no-scrollbar'>
       {storage.get().map(({ id, data }) => (
         <Item key={id} id={id} text={data} />
       ))}
